61ffa1051572c640e1ec93519a151679d3e179d0
Daniil Bolotov <somerapmails@yandex.ru>
2021-03-06 23:31:58 +0300

    configured webclient

--- src/main/java/com/exyfi/reactive/shop/config/AppConfig.java
+++ src/main/java/com/exyfi/reactive/shop/config/AppConfig.java
@@ -1,14 +1,34 @@
 package com.exyfi.reactive.shop.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AppConfig {
 
+    public static final int TIMEOUT = 1000;
+
     @Bean
     public WebClient webClient() {
-        return WebClient.create();
+        final var tcpClient = TcpClient
+                .create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
+                .doOnConnected(connection -> {
+                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                });
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+                .build();
     }
 }
